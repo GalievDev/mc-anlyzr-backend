61f7d7b8212997b564805807ee08452761cc025d
galiev <ggvalion@gmail.com>
2024-08-04 18:48:37 +0500

    Fetching every 5 seconds

--- src/main/kotlin/dev/galiev/anlyzr/client/ModrinthFetcher.kt
+++ src/main/kotlin/dev/galiev/anlyzr/client/ModrinthFetcher.kt
@@ -2,14 +2,15 @@ package dev.galiev.anlyzr.client
 
 import dev.galiev.anlyzr.dto.Project
 import io.ktor.client.*
-import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withTimeout
 import kotlinx.serialization.json.Json
+import kotlin.time.Duration.Companion.milliseconds
 
 object ModrinthFetcher {
     const val URL = "https://api.modrinth.com/v2"
@@ -26,13 +27,16 @@ object ModrinthFetcher {
 
     }
 
-
-    suspend fun getProjects(): List<Project> {
-        val response: HttpResponse = client.get("$URL/user/GalievDev/projects") {
-            contentType(ContentType.Application.Json)
+    suspend fun fetchData(): List<Project> {
+        var count = 0
+        withTimeout(5000.milliseconds) {
+            while (true) {
+                count++
+                client.get("$URL/user/GalievDev/projects") {
+                    contentType(ContentType.Application.Json)
+                }
+                delay(1000.milliseconds)
+            }
         }
-
-        val projects: List<Project> = response.body()
-        return projects
     }
 }
--- src/main/kotlin/dev/galiev/anlyzr/plugins/Routing.kt
+++ src/main/kotlin/dev/galiev/anlyzr/plugins/Routing.kt
@@ -1,6 +1,5 @@
 package dev.galiev.anlyzr.plugins
 
-import dev.galiev.anlyzr.client.ModrinthFetcher
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -8,7 +7,7 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respond(ModrinthFetcher.getProjects())
+            call.respond("Hello World!")
         }
     }
 }
