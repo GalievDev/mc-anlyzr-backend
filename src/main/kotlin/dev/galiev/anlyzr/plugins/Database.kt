101ac482c5514ce73901476036cdc6aa0f5c3845
Vazir Vadirov <ggvalion@gmail.com>
2024-10-07 21:56:31 +0300

    Removed unnecessary functions

--- src/main/kotlin/dev/galiev/anlyzr/plugins/Database.kt
+++ src/main/kotlin/dev/galiev/anlyzr/plugins/Database.kt
@@ -2,43 +2,15 @@ package dev.galiev.anlyzr.plugins
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
-import org.intellij.lang.annotations.Language
-import java.sql.ResultSet
+import org.jetbrains.exposed.sql.Database
 
-object Database {
-    private val dataSource: HikariDataSource by lazy {
-        HikariDataSource(HikariConfig().apply {
-            driverClassName = "org.postgresql.Driver"
-            jdbcUrl = "jdbc:postgresql://$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
-            username = POSTGRES_USER
-            password = POSTGRES_PASS
-            transactionIsolation = "TRANSACTION_REPEATABLE_READ"
-            validate()
-        })
-    }
-
-    fun dataStore(): HikariDataSource = dataSource
-
-    fun query(@Language("PostgreSQL") sql: String, resultSet: (ResultSet) -> Unit) {
-        runCatching {
-            dataStore().connection.use { connection ->
-                connection.createStatement().use { statement ->
-                    statement.executeQuery(sql).use { resultSet ->
-                        resultSet(resultSet)
-                    }
-                }
-            }
-        }.onFailure { println(it.localizedMessage) }
-    }
-
-    fun update(@Language("PostgreSQL") sql: String): Int {
-        runCatching {
-            dataStore().connection.use { connection ->
-                connection.createStatement().use { statement ->
-                    return statement.executeUpdate(sql)
-                }
-            }
-        }.onFailure { println(it.localizedMessage) }
-        return -1
-    }
+fun configureDatabase() {
+    Database.connect(HikariDataSource(HikariConfig().apply {
+        driverClassName = "org.postgresql.Driver"
+        jdbcUrl = "jdbc:postgresql://$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
+        username = POSTGRES_USER
+        password = POSTGRES_PASS
+        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        validate()
+    }))
 }
