61e3b98107d7d75354ca3b440c5678427653c19a
Vazir Vadirov <ggvalion@gmail.com>
2024-10-07 21:55:18 +0300

    Added Exposed

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ val kotlin_version: String by project
 val logback_version: String by project
 val postgres_version: String by project
 val h2_version: String by project
+val exposed_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.0"
@@ -30,6 +31,9 @@ dependencies {
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposed_version")
     implementation("org.postgresql:postgresql:$postgres_version")
     implementation("com.h2database:h2:$h2_version")
     implementation("io.ktor:ktor-server-netty-jvm")
