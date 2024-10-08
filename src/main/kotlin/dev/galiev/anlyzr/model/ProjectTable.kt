007ab29ed4d9b2b24c7a8447c2a2e621028769ea
Vazir Vadirov <ggvalion@gmail.com>
2024-10-08 17:45:47 +0300

    Renaming

--- src/main/kotlin/dev/galiev/anlyzr/model/ProjectTable.kt
+++ src/main/kotlin/dev/galiev/anlyzr/model/ProjectTable.kt
@@ -2,7 +2,7 @@ package dev.galiev.anlyzr.model
 
 import org.jetbrains.exposed.dao.id.IntIdTable
 
-object ProjectModel: IntIdTable("projects") {
+object ProjectTable: IntIdTable("projects") {
     val title = varchar("title", 255)
     val description = text("description")
 }
--- src/main/kotlin/dev/galiev/anlyzr/model/StatsTable.kt
+++ src/main/kotlin/dev/galiev/anlyzr/model/StatsTable.kt
@@ -4,9 +4,9 @@ import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.kotlin.datetime.timestampWithTimeZone
 
-object StatsModel: Table("stats") {
+object StatsTable: Table("stats") {
     val time = timestampWithTimeZone("time")
-    val projectId = integer("project_id").references(ProjectModel.id, ReferenceOption.CASCADE, ReferenceOption.CASCADE)
+    val projectId = integer("project_id").references(ProjectTable.id, ReferenceOption.CASCADE, ReferenceOption.CASCADE)
     val title = varchar("title", 255)
     val downloads = integer("downloads")
     val followers = integer("followers")
