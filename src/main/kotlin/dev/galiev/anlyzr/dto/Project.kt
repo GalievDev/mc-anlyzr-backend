1d9c60319fbb38b14296bff3e0f8ac44981adfbe
Vazir Vadirov <ggvalion@gmail.com>
2024-10-08 17:53:33 +0300

    Updated project DTO

--- src/main/kotlin/dev/galiev/anlyzr/dto/Project.kt
+++ src/main/kotlin/dev/galiev/anlyzr/dto/Project.kt
@@ -1,6 +1,6 @@
 package dev.galiev.anlyzr.dto
 
-import dev.galiev.anlyzr.model.ProjectModel
+import dev.galiev.anlyzr.model.ProjectTable
 import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.sql.ResultRow
 
@@ -9,12 +9,14 @@ data class Project(
     var projectId: Int = -1,
     val title: String = "",
     val description: String = "",
+    val downloads: Int = 0,
+    val followers: Int = 0
 ) {
     companion object {
         fun fromResultRow(resultRow: ResultRow): Project = Project(
-            projectId = resultRow[ProjectModel.id].value,
-            title = resultRow[ProjectModel.title],
-            description = resultRow[ProjectModel.description],
+            projectId = resultRow[ProjectTable.id].value,
+            title = resultRow[ProjectTable.title],
+            description = resultRow[ProjectTable.description],
         )
     }
 }
