35ff822ef14308447fdddd130b682e2544d9678f
Vazir Vadirov <ggvalion@gmail.com>
2024-10-19 19:27:14 +0300

    DTO Updated

--- src/main/kotlin/dev/galiev/anlyzr/dto/Stats.kt
+++ src/main/kotlin/dev/galiev/anlyzr/dto/Stats.kt
@@ -1,11 +1,24 @@
 package dev.galiev.anlyzr.dto
 
+import dev.galiev.anlyzr.model.StatsTable
 import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.sql.ResultRow
 
 @Serializable
 data class Stats (
+    val time: Long,
     val projectId: Int = 0,
     val title: String = "",
     val downloads: Int = 0,
     val followers: Int = 0
-)
+) {
+    companion object {
+        fun fromResultRow(resultRow: ResultRow): Stats = Stats(
+            time = resultRow[StatsTable.time].second.toLong(),
+            projectId = resultRow[StatsTable.projectId],
+            title = resultRow[StatsTable.title],
+            downloads = resultRow[StatsTable.downloads],
+            followers = resultRow[StatsTable.followers]
+        )
+    }
+}
