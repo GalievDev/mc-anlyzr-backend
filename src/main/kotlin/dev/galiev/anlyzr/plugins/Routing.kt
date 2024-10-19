cfa100e0073a9302db9f785d58a12916846ef608
Vazir Vadirov <ggvalion@gmail.com>
2024-10-19 19:27:55 +0300

    Route for method

--- src/main/kotlin/dev/galiev/anlyzr/plugins/Routing.kt
+++ src/main/kotlin/dev/galiev/anlyzr/plugins/Routing.kt
@@ -1,13 +1,28 @@
 package dev.galiev.anlyzr.plugins
 
+import dev.galiev.anlyzr.repository.postgre.PostgresStatsRepository
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.datetime.Clock
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respond("Hello World!")
+        }
+        route("/stats") {
+            get("/{id}") {
+                val id = call.parameters["id"]?.toIntOrNull() ?: return@get call.respond(
+                    HttpStatusCode.BadRequest, "Bad Request"
+                )
+
+                val from = call.parameters["from"]?.toLongOrNull()
+                val to = call.parameters["to"]?.toLongOrNull() ?: Clock.System.now().toEpochMilliseconds()
+
+                call.respond(PostgresStatsRepository.getInDateRange(id, from!!, to))
+            }
         }
     }
 }
