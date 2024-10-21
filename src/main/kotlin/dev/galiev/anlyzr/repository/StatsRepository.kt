d4cfb722f4131c593497bc99c6b893442faf8011
Vazir Vadirov <ggvalion@gmail.com>
2024-10-21 18:19:22 +0300

    Renaming

--- src/main/kotlin/dev/galiev/anlyzr/repository/StatsRepository.kt
+++ src/main/kotlin/dev/galiev/anlyzr/repository/StatsRepository.kt
@@ -4,6 +4,6 @@ import dev.galiev.anlyzr.dto.Stats
 
 interface StatsRepository {
     suspend fun addStat(stats: Stats): Int
-    suspend fun getInDateRange(id: Int, start: Long, end: Long): List<Stats>
+    suspend fun getInDateRange(id: Int, from: Long, to: Long): List<Stats>
     suspend fun getStatsByProjectId(id: Int): List<Stats>
 }
